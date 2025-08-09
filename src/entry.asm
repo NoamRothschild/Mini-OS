2a89999c5b794834395f0c3e0db46e9c0d43ea47
Noam Rothschild <noamr.lego@gmail.com>
2025-08-09 23:31:28 +0300

    switched to multiboot2

--- src/entry.asm
+++ src/entry.asm
@@ -12,6 +12,8 @@ section .data
     stack_len dd stack_size
 
 section .multiboot
+%define MBOOT2
+%ifdef MBOOT1
     ALIGNMENT equ 1 << 0
     MEMINFO equ 1 << 1
     MBOOT_MAGIC equ 0x1BADB002
@@ -21,6 +23,23 @@ section .multiboot
     dd FLAGS                  ; flags
     dd -(MBOOT_MAGIC + FLAGS) ; checksum
     dd 0                      ; padding
+%elifdef MBOOT2
+    multiboot_start:
+        MBOOT_MAGIC equ 0xe85250d6
+        MBOOT_LENGTH equ multiboot_end - multiboot_start
+
+        dd MBOOT_MAGIC ; multiboot2
+        dd 0 ; protected mode i386
+        dd MBOOT_LENGTH ; header length
+        dd 0x100000000 - (MBOOT_MAGIC + 0 + (MBOOT_LENGTH)) ; checksum
+
+        ; end tag
+        dd 0
+        dd 8
+    multiboot_end:
+%else
+    %error "EITHER MBOOT1 OR MBOOT2 MUST BE SELECTED, ALSO UPDATE grub.cfg ACORDINGLY"
+%endif
 
 section .text
 bits 32
