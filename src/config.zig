e6d0b4f792fd7a361c64d1cb5f4c73349bd365ce
Noam Rothschild <noamr.lego@gmail.com>
2025-11-21 20:55:32 +0200

    fix: increased stack size to overcome overflow && moved syscall number to config.zig

--- src/config.zig
+++ src/config.zig
@@ -1,4 +1,5 @@
 const kilobyte: comptime_int = 1024;
 const megabyte: comptime_int = kilobyte * kilobyte;
 
-pub const stack_size = 2 * kilobyte;
+pub const stack_size = 16 * kilobyte;
+pub const syscall_number = 0x90; // 144 :)
