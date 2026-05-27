6317b38f347f28a29a5171a05808d764ff6740b7
NoamRothschild <noamr.lego@gmail.com>
2026-05-27 22:57:59 +0300

    stopped being a copycat and implement paging alone

--- src/arch/x86/entry.zig
+++ src/arch/x86/entry.zig
@@ -18,65 +18,92 @@ export const multiboot_header: MultiBootHeaderV2 align(4) linksection(".multiboo
 pub export var stack_bottom: [16 * 4096]u8 align(16) linksection(".bss") = undefined;
 pub export var stack_top = &stack_bottom[stack_bottom.len - 4];
 
-/// 0xC0000000 - start of kernel in virtual memory
-/// 22 - starting bit of addr in a PDE
-const first_kernel_dir = 0xC0000000 >> 22;
-export var boot_page_table align(4096) linksection(".boot") = init: {
-    @setEvalBranchQuota(1024);
-    var dir: paging.PageTable = undefined;
+const higher_half_base = 0xC0000000;
+const page_dir_idx_base = (higher_half_base >> 22);
+const kernel_page_table_count = 1024 - page_dir_idx_base;
 
-    const kernel_page = paging.PageTableEntry{
-        .present = 1,
-        .permission = .readwrite,
-        .access = .supervisor,
-        .page_write_through = 0,
-        .page_cache_disable = 0,
-        .page_size = .@"4 MiB",
-        .physical_addr = 0,
-    };
+// we need to map 0xC0000000 to 0xFFFFFFFF as kernel
+var page_tables: [kernel_page_table_count][1024]paging.PageTableEntry align(0x1000) linksection(".boot") = init: {
+    @setEvalBranchQuota(1000000);
+    var tables: [kernel_page_table_count][1024]paging.PageTableEntry = undefined;
 
-    dir[0] = kernel_page;
-
-    var idx = 1;
-    for (0..first_kernel_dir - 1) |_| {
-        dir[idx] = @bitCast(@as(u32, 0));
-        idx += 1;
+    for (&tables, page_dir_idx_base..) |*page_table, pde_idx| {
+        for (0..1024) |pte_idx| {
+            page_table.*[pte_idx] = paging.kernel_table_page;
+            page_table.*[pte_idx].physical_addr = @truncate(pte_idx | (pde_idx << 10) - (higher_half_base >> 12));
+            // NOTE: we subtract higher_half_base to account for the LMA changes in the linker.ld like `AT(ADDR(.text) - 0xC0000000)`
+        }
     }
 
-    // map 0xC0000000 to 0xFFFFFFFF as kernel
-    for (0..1024 - first_kernel_dir) |i| {
-        dir[idx] = kernel_page;
-        dir[idx].physical_addr = @truncate(i << 10);
-        idx += 1;
-    }
-
-    break :init dir;
+    break :init tables;
 };
 
+// map the first 8 MB of memory (identity mapped)
+var boot_page_tables: [2][1024]paging.PageTableEntry align(0x1000) linksection(".boot") = init: {
+    @setEvalBranchQuota(1000000);
+
+    var tables: [2][1024]paging.PageTableEntry = undefined;
+    for (&tables, 0..) |*table, pde_idx| {
+        for (0..1024) |pte_idx| {
+            table[pte_idx] = paging.kernel_table_page;
+            table[pte_idx].physical_addr = @truncate(pte_idx | pde_idx << 10);
+            table[pte_idx].global = 1; // 'Global' tells the processor not to invalidate the TLB entry corresponding to the page upon a MOV to CR3 instruction.
+            // // NOTE: I enabled Global here, but I don't think its system flag is enabled, so this does not do anything useful for now.
+        }
+    }
+
+    break :init tables;
+};
+
+var page_directories: [1024]paging.PageDirectoryEntry align(0x1000) linksection(".boot") = undefined;
+
+pub fn init_page_directories() linksection(".boot") callconv(.C) void {
+    for (0..2) |i| {
+        page_directories[i] = paging.kernel_directory_page;
+        page_directories[i].page_table_physical_addr = @truncate(@intFromPtr(&boot_page_tables[i]) >> 12);
+    }
+
+    for (2..page_dir_idx_base - 1) |pde_idx| {
+        const pde = &page_directories[pde_idx];
+        pde.* = paging.non_present_directory_page;
+    }
+
+    for (page_dir_idx_base..1024) |pde_idx| {
+        const pde = &page_directories[pde_idx];
+        const page_table_addr = &page_tables[pde_idx - page_dir_idx_base];
+        pde.* = paging.kernel_directory_page;
+        pde.*.page_table_physical_addr = @truncate(@intFromPtr(page_table_addr) >> 12);
+    }
+}
+
 export fn _start() align(16) linksection(".boot") callconv(.naked) noreturn {
-    // set up page directory,
-    // turning on 4 MiB pages
-    // and enabling paging
-    asm volatile (
-        \\ mov %ecx, %cr3
-        \\
-        \\ mov %cr4, %ecx
-        \\ or $0x00000010, %ecx
-        \\ mov %ecx, %cr4
-        \\
-        \\ mov %cr0, %ecx
-        \\ or $0x80000000, %ecx
-        \\ mov %ecx, %cr0
-        \\
-        \\ mov %eax, %ebx
+    // enable paging
+    asm volatile ("call %[init_page_dir:P]"
         :
-        : [boot_page_table] "{ecx}" (&boot_page_table),
-        : "ecx"
+        : [init_page_dir] "X" (&init_page_directories),
+    );
+
+    asm volatile ("call %[breakpoint:P]"
+        :
+        : [breakpoint] "X" (&bp),
+    );
+
+    asm volatile (
+        \\ mov %eax, %cr3
+        \\
+        \\ mov %cr0, %eax
+        \\ or $0x80000001, %eax
+        \\ mov %eax, %cr0
+        :
+        : [page_directories] "{eax}" (&page_directories),
+        : "eax"
     );
 
     asm volatile ("jmp higher_half_kernel");
     while (true) {}
 }
+
+export fn bp() linksection(".boot") callconv(.C) void {}
 
 export fn higher_half_kernel() align(16) callconv(.C) noreturn {
     asm volatile ("mov %[stack_top], %esp"
--- src/arch/x86/paging.zig
+++ src/arch/x86/paging.zig
@@ -1,6 +1,6 @@
 pub const PageSize = enum(u1) { @"4 KiB" = 0, @"4 MiB" = 1 };
 
-pub const PageTableEntry = packed struct {
+pub const PageDirectoryEntry = packed struct {
     present: u1 = 0, // must be 1 to map a page
     permission: enum(u1) { readonly = 0, readwrite = 1 }, // if 0, writes may not be allowed to the page referenced by this entry
     access: enum(u1) { user = 1, supervisor = 0 }, // if 0, user-mode (ring-3) accesses are not allowed to the page referenced by this entry
@@ -10,16 +10,21 @@ pub const PageTableEntry = packed struct {
     dirty: u1 = 0, // indicates whether software has written to the page referenced by this entry
     page_size: PageSize,
     available: u4 = 0,
-    physical_addr: u20,
+    page_table_physical_addr: u20,
 };
 
-pub const PageTable = [1024]PageTableEntry;
-
-/// A struct used by the kernel writer and not passed directly by the CPU
-pub const PageDirectory = struct {
-    tables: [1024]*PageTableEntry,
-    physical_tables: PageTable, // should be passed to the CPU inside the cr3 reg
-    physical_addr: u32,
+pub const PageTableEntry = packed struct {
+    present: u1 = 0, // must be 1 to map a page
+    permission: enum(u1) { readonly = 0, readwrite = 1 }, // if 0, writes may not be allowed to the page referenced by this entry
+    access: enum(u1) { user = 1, supervisor = 0 }, // if 0, user-mode (ring-3) accesses are not allowed to the page referenced by this entry
+    page_write_through: u1, // indirectly determines the memory type used to access the page referenced by this entry
+    page_cache_disable: u1, // indirectly determines the memory type used to access the page referenced by this entry
+    adressed: u1 = 0, // indicates whether software has accessed the page referenced by this entry
+    dirty: u1 = 0, // indicates whether software has written to the page referenced by this entry
+    page_attribute: u1,
+    global: u1,
+    available: u3 = 0,
+    physical_addr: u20,
 };
 
 comptime {
@@ -27,6 +32,27 @@ comptime {
         @compileError("PageTableEntry must be 32 bits.");
 }
 
-pub var current_directory: PageDirectory = undefined;
+pub const kernel_table_page: PageTableEntry = .{
+    .present = 1,
+    .permission = .readwrite,
+    .access = .supervisor,
+    .page_write_through = 0,
+    .page_cache_disable = 0,
+    .page_attribute = 0,
+    .global = 0,
+    .physical_addr = 0,
+};
+
+pub const kernel_directory_page: PageDirectoryEntry = .{
+    .present = 1,
+    .permission = .readwrite,
+    .access = .supervisor,
+    .page_write_through = 0,
+    .page_cache_disable = 0,
+    .page_size = .@"4 KiB",
+    .page_table_physical_addr = 0,
+};
+
+pub const non_present_directory_page: PageDirectoryEntry = @bitCast(@as(u32, 0));
 
 pub fn init() void {}
